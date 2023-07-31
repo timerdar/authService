5cafdc667d841fb7bb2d5fd25f936f4a17775a6f
timerdar <timergazin24@mail.ru>
2023-07-31 18:38:34 +0500

    Add GET method

--- src/main/java/com/practice/authService/AuthController.java
+++ src/main/java/com/practice/authService/AuthController.java
@@ -3,6 +3,7 @@ package com.practice.authService;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import com.practice.authService.models.GetAns;
 import com.practice.authService.models.PostAns;
 import com.practice.authService.models.User;
 
@@ -13,13 +14,17 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class AuthController {
 
-    @PostMapping("/auth")
-    public ResponseEntity<PostAns> getLoginData(@RequestBody User user){
+    @PostMapping
+    public ResponseEntity<PostAns> Post(@RequestBody User user){
         Date date = new Date();
         SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
         PostAns res = new PostAns(user.login(), formatter.format(date));
         return ResponseEntity.ok(res);
     }
 
-
+    @GetMapping
+    public ResponseEntity<GetAns> Get(){
+        GetAns ans = new GetAns("It`s a GET response message!");
+        return ResponseEntity.ok(ans);
+    }
 }
---
+++ src/main/java/com/practice/authService/models/GetAns.java
@@ -0,0 +1,3 @@
+package com.practice.authService.models;
+
+public record GetAns(String mess) { }
