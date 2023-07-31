356ba3526cb4fe52ee673d5427354ac42a4212ba
timerdar <timergazin24@mail.ru>
2023-07-31 18:13:49 +0500

    Add POST method

--- src/main/java/com/practice/authService/AuthController.java
+++ src/main/java/com/practice/authService/AuthController.java
@@ -6,17 +6,20 @@ import java.util.Date;
 import com.practice.authService.models.PostAns;
 import com.practice.authService.models.User;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping
 @RestController
 public class AuthController {
 
-    @PostMapping("/")
-    public PostAns getLoginData(User user){
-        return PostAns(user.login(), )
+    @PostMapping("/auth")
+    public ResponseEntity<PostAns> getLoginData(@RequestBody User user){
+        Date date = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+        PostAns res = new PostAns(user.login(), formatter.format(date));
+        return ResponseEntity.ok(res);
     }
+
+
 }
